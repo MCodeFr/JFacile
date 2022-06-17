5b7966e37d819f50997a0178c4632c716d02c7d8
MCodeFr <87140722+MCodeFr@users.noreply.github.com>
2022-06-17 08:39:52 +0200

    Update faker.ts

--- src/faker.ts
+++ src/faker.ts
@@ -2,7 +2,7 @@ interface Color {
   name: string;
   code: string;
 }
-
+  
 const JColor: Color[] = [
     {name: "brightred", code: "#E74C3C"},
     {name: "soothingpurple", code: "#9B59B6"},
@@ -12,74 +12,34 @@ const JColor: Color[] = [
     {name: "groovygray", code: "#D7DBDD"}
   ];
 
+const JArray = [
+  "brightred, #E74C3C",
+  "soothingpurple, #9B59B6",
+  "skyblue, #5DADE2",
+  "leafygreen, #48C9B0",
+  "sunkissedyellow, #F4D03F",
+  "groovygray, #D7DBDD"
+];
+
   export function JColorRandom(): Color {
     return JColor[Math.floor(Math.random() * JColor.length)];
 }
 
-const JExitTest = process.on("exit", (code) => {
-    console.log(`Le processuss s'est arrêté avec le code ${code} !`);
-  });
-
-export const JExit = () => {
-    return(JExitTest)
+export const JColorArray = () => {
+  console.log(JArray)
 }
 
-const JUnhandledRejectonTest = () => {
-    process.on(
-        "unhandledRejection",
-        (reaseon, promise) =>
-          console.log(
-            `/!/ UnhandledRejection/!/\n Raison : ${reaseon}\n`,
-            `Promise : ${promise}`
-          )
-      );
-}
-
-export const JUnhandledRejection = () => {
-    return(JUnhandledRejectonTest)
-}
-
-const JUnCaughtExceptionTest = () => {
-    process.on("uncaughtException", (err, origin) => {
-        console.log(
-          `/!/ Unchaught Exception /!/\nErreur : ${err}`,
-          `Origine : ${origin}`
-        );
-      });
-}
-export const JUnCaughtException = () => {
-    return(JUnCaughtExceptionTest)
-}
-const JWarningTest = () => {
-    process.on("warning", (...args) => console.log(...args));
-}
-export const JWarning = () => {
-    return(JWarningTest)
-}
+//In progress...
 const JDateTest = () => {
-  switch (new Date().getDay()) {
-    case 1: "On est lundi !"
-    break;
-    case 2: "On est mardi !"
-    break;
-    case 3: "On est mercredi !"
-    break;
-    case 4: "On est jeudi !"
-    break;
-    case 5: "On est vendredi !"
-    break;
-    case 6: "On est samedi !"
-    break;
-    case 7: "On est dimanche !"
-  }
+  return (new Date().getDay()) 
 }
 export const JDate = () => {
   return (JDateTest)
 }
 const JLogTest = () => {
-  console.log()
+  return (console.log())
 }
-export const JLog = () => {
+export function JLog() {
   return(JLogTest)
 }
 const JRTest = () => {
