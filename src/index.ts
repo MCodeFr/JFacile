eea8505012c2a31848af0f36c3c91070a957abf0
MCodeFr <87140722+MCodeFr@users.noreply.github.com>
2022-06-17 08:40:55 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { JColorRandom } from "./faker";
-import { JExit } from "./faker";
-import { JUnhandledRejection } from "./faker";
-import { JWarning } from "./faker";
-import { JUnCaughtException } from "./faker";
+import { JExit } from "./process";
+import { JUnhandledRejection } from "./process";
+import { JWarning } from "./process";
+import { JUnCaughtException } from "./process";
 import { JDate } from "./faker";
 import { JR } from "./faker";
 import { JLog } from "./faker";
 import { JVar } from "./faker";
+import {JRejectionHandled} from "./process"
+import {JColorArray} from "./faker"
 
-export {JColorRandom}
+exports = {
+    get JColorRandom() {return JColorRandom()},
+    get JDate() {return JDate()},
+    get JLog() {return JLog}
+}
+export {JColorArray} 
 export {JExit}
 export {JUnhandledRejection}
 export {JWarning}
 export {JUnCaughtException}
-export {JDate}
 export {JR}
-export {JLog}
 export {JVar} 
+export {JRejectionHandled}
