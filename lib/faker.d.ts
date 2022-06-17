bc5ddf6e12b05c0ec546a9fa7b8119dc287e7bc7
MCodeFr <87140722+MCodeFr@users.noreply.github.com>
2022-06-17 08:41:24 +0200

    Update faker.d.ts

--- lib/faker.d.ts
+++ lib/faker.d.ts
@@ -1,15 +1,11 @@
-/// <reference types="node" />
 interface Color {
     name: string;
     code: string;
 }
 export declare function JColorRandom(): Color;
-export declare const JExit: () => NodeJS.Process;
-export declare const JUnhandledRejection: () => () => void;
-export declare const JUnCaughtException: () => () => void;
-export declare const JWarning: () => () => void;
-export declare const JDate: () => () => void;
-export declare const JLog: () => () => void;
+export declare const JColorArray: () => void;
+export declare const JDate: () => () => number;
+export declare function JLog(): () => void;
 export declare const JR: () => () => void;
 export declare const JVar: () => () => void;
 export {};
