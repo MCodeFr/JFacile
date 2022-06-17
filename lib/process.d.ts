39c81b2d3aff3c81881f6863fc8ccd855754d991
MCodeFr <87140722+MCodeFr@users.noreply.github.com>
2022-06-17 08:43:09 +0200

    Add files via upload

---
+++ lib/process.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+export declare const JExit: () => NodeJS.Process;
+export declare const JUnhandledRejection: () => () => void;
+export declare const JUnCaughtException: () => () => void;
+export declare const JWarning: () => () => void;
+export declare const JRejectionHandled: () => () => void;
